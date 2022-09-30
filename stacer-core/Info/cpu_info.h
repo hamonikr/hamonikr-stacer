e52a81b76449cc163817fd6684190bc84f6efaf2
JamesBae <bdh1993@invesume.com>
2022-09-30 18:51:38 +0900

    Fix CPU Speed

--- stacer-core/Info/cpu_info.h
+++ stacer-core/Info/cpu_info.h
@@ -10,7 +10,7 @@
 #define PROC_LOADAVG "/proc/loadavg"
 #define PROC_STAT    "/proc/stat"
 #define LSCPU_COMMAND "LANG=nl_NL.UTF-8 lscpu"
-#define CPU_SPEED_COMMAND "LANG=nl_NL.UTF-8 journalctl --boot | grep 'kernel: tsc: Detected' -i | awk '{print $8}'"
+#define CPU_SPEED_COMMAND "LANG=nl_NL.UTF-8 journalctl --boot | grep 'kernel: tsc: Detected' -i | awk '{print $8}' | tail -1"
 #define CPU_CORE_COUNT "LANG=nl_NL.UTF-8 lscpu | grep -E '^Core' | awk '{print $4}'"
 #define CPU_THREAD_COUNT "LANG=nl_NL.UTF-8 lscpu | grep -E '^CPU\\(' | awk '{print $2}'"
 
